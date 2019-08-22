ebbe6aafdc64b0a424b4218d64b075657aa4e75c
jefferson <jeff.otoni@gmail.com>
2019-08-22 03:20:50 -0300

    instalador em sh bash

--- v1/install.sh
+++ v1/install.sh
@@ -28,7 +28,7 @@ fi
 
 sudo rm -rf $DIR
 sudo mkdir -p $DIR
-sudo wget -c "https://raw.githubusercontent.com/jeffotoni/s3godo/master/spaces/copyspace/v2/copyspace" -P "$DIR"
+sudo wget -c "https://raw.githubusercontent.com/jeffotoni/copyspace/master/v2/copyspace" -P "$DIR"
 echo "..."
 sleep 1
 sudo chmod 755 -R $DIR2
